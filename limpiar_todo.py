3b841804afabffaf1aac76503f83175746598e1c
JABV75 <93306875+JABV75@users.noreply.github.com>
2023-11-29 19:04:34 -0600

    update

--- limpiar_todo.py
+++ limpiar_todo.py
@@ -1,51 +1,53 @@
+#Aquí se importan las librerías a utilizar
 import pandas as pd
 import chardet
 
-# Function to detect the encoding of a file
+#En esta sección se crea la función para detectar la codificación de los caracteres en los datos
 def detect_encoding(file_path):
     with open(file_path, 'rb') as f:
         result = chardet.detect(f.read())
     return result['encoding']
 
+#En esta sección se especifica el nombre de los archivos a leer
+
 x = ['2019_01', '2019_02', '2020_01', '2020_02', '2021_01', '2021_02', '2022_01', '2022_02', '2023_01', '2023_02']
 
-# Initialize an empty DataFrame to store concatenated data
-all_data = pd.DataFrame()
+#En esta sección se crea un DataFrame vacío para concatenar los datos en caso de utilizarse
 
-# Chunk size for reading CSV files
 chunk_size = 90000
-cont =0
-# Loop through each file
+
+#En esta sección se itera sobre los archivos a leer
 for i in x:
     
     #open the file in windows
-    file_path = 'D:\\UsX\\Escritorio\\data\\' + i + '.csv'
+    #file_path = 'D:\\UsX\\Escritorio\\data\\' + i + '.csv'
     
     #open the file in mac
-    #file_path = '/Users/josebenvenuto/Desktop/data/' + i + '.csv'
-    cont = cont + 1
+    file_path = '/Users/josebenvenuto/Desktop/data/' + i + '.csv'
+    
     print(i)
-    # Detect encoding
+    
+    #Aqui se detecta la codificación de los caracteres en los datos
     encoding = detect_encoding(file_path)
 
-    # Read CSV with detected encoding using chunks
+    # Leer el archivo en chunks
     chunks = pd.read_csv(file_path, encoding=encoding, chunksize=chunk_size, low_memory=False)
 
-    # Process each chunk
+    # Aqui se itera sobre los chunks para limpiar los datos
     for chunk in chunks:
 
         
         # Convert only string columns to UTF-8 encoding
         chunk = chunk.apply(lambda x: x.str.encode('utf-8').str.decode('utf-8') if x.dtype == 'O' else x)
 
-        # Continue with your data cleaning and manipulation steps
+        # Aqui se eliminan las columnas que no se utilizarán
         chunk = chunk.drop(['SOURCE_NAME', 'SITE_USE_CODE', 'LOCATION', 'TAX_CODE', 'SITE_SHIP_TO', 'LOCATION_SHIP_TO',
                             'TAX_SHIP_TO', 'ATTRIBUTE10', 'WAREHOUSE_ID', 'TRX_NUMBER', 'LINE_NUMBER', 'SALES_ORDER', 'SALES_ORDER_LINE', 'SALES_ORDER_SOURCE',
                             'TAX_RATE', 'ORG_ID', 'INTERFACE_HEADER_ATTRIBUTE2', 'CUST_TRX_TYPE_ID', 'NAME', 'TYPE', 'INVOICE_CURRENCY_CODE', 'INVENTORY_ITEM_ID',
                             'GL_DATE', 'TAX_CLASSIFICATION_CODE', 'QUANTITY_CREDITED', 'UNIT_SELLING_PRICE', 'EXCHANGE_RATE', 'CUSTOMER_TRX_ID',
                             'CUSTOMER_TRX_LINE_ID', 'PARTY_SITE_ID', 'SEGMENT4', 'X'], axis=1)
 
-        # Make the columns of the new CSV cast for each type
+        # Aqui se cambian los tipos de datos de las columnas
         chunk['CUST_ACCOUNT_ID'] = pd.to_numeric(chunk['CUST_ACCOUNT_ID'], errors='coerce').astype('Int64')
         chunk['PARTY_NAME'] = chunk['PARTY_NAME'].astype(str)
         chunk['SALES_CHANNEL_CODE'] = chunk['SALES_CHANNEL_CODE'].astype(str)
@@ -55,26 +57,9 @@ for i in x:
         #chunk['TRX_DATE'] = pd.to_datetime(chunk['TRX_DATE'], format='%d-%b-%Y %H:%M:%S', errors='coerce').dt.strftime('%Y/%m/%d')
         chunk['QUANTITY_INVOICED'] = pd.to_numeric(chunk['QUANTITY_INVOICED'], errors='coerce').astype('float64')
         chunk['EXTENDED_AMOUNT'] = pd.to_numeric(chunk['EXTENDED_AMOUNT'], errors='coerce').astype('float64')
-        # Concatenate the current chunk with the previous ones
-        #all_data = pd.concat([all_data, chunk], ignore_index=True)
-        
-        #save the chunk for 2 years in a new csv
-        # all_data = pd.concat([all_data, chunk], ignore_index=True)
-        
-        # if(cont == 2):
-        #     all_data.to_csv('/Users/josebenvenuto/Desktop/data/'+ i +'_clean.csv', index=False, encoding='utf-8')
-        #     print('2 years')
-        #     cont = 0
-        #     #clean all_data
-        #     all_data = pd.DataFrame()
 
         #save the chunk in a new csv in windows
-        chunk.to_csv('D:\\UsX\\Escritorio\\data\\' + i + '_clean.csv', index=False, encoding='utf-8')
+        #chunk.to_csv('D:\\UsX\\Escritorio\\data\\' + i + '_clean.csv', index=False, encoding='utf-8')
         
         #save the chunk in a new csv in mac
-        #chunk.to_csv('/Users/josebenvenuto/Desktop/data/' + i + '_clean.csv', index=False, encoding='utf-8')
-        
-# ... other data cleaning steps ...
-
-# Save the concatenated and cleaned DataFrame to a new CSV file with UTF-8 encoding
-#all_data.to_csv('D:\\UsX\\Escritorio\\data\\cleaned_file.csv', index=False, encoding='utf-8')
+        chunk.to_csv('/Users/josebenvenuto/Desktop/data/' + i + '_clean.csv', index=False, encoding='utf-8')
